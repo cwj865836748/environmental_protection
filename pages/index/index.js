a01fec6eb83cfecb2f15b31dc2867e60b0149010
username <email>
2020-07-31 10:01:52 +0800

    环保会展小程序初始

---
+++ pages/index/index.js
@@ -0,0 +1,83 @@
+const App=getApp()
+var utils = require('../../utils/util.js')
+import {navigateTo} from '../../utils/wx.js'
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+   
+  },
+
+  //获取当前位置
+  getLocation(){
+    utils.getAuth().then(re=>{
+      console.log(re)
+    })
+  },
+  //跳转路径
+  goSearch(){
+    navigateTo('/pages/keywordSearch/keywordSearch')
+  },
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    this.getLocation()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
